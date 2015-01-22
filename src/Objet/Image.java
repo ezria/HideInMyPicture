5a5d770a6a8f75e45bfa7cb6f7ec2e71cbc5e7d8
ezria <corentinchevallier@gmail.com>
2015-01-22 20:31:27 +0100

    delete useless object

--- src/Objet/Image.java
+++ src/Objet/Image.java
@@ -3,18 +3,15 @@ package Objet;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.Raster;
-import java.util.ArrayList;
 
 public class Image {
 	private BufferedImage buffImage;
 	private Raster r;
 	private ColorModel cm;
-	private ArrayList<Pixel> pixels = null;
 	
 	public Image (BufferedImage buffImage){
 		this.buffImage = buffImage;
 		r = this.buffImage.getData();
-		System.out.println(buffImage.getRGB(0, 0));
 		cm = this.buffImage.getColorModel(); 
 	}
 
@@ -40,30 +37,5 @@ public class Image {
 
 	public void setCm(ColorModel cm) {
 		this.cm = cm;
-	}
-	
-	public ArrayList<Pixel> getPixels(){
-		if (pixels == null){
-			pixels = new ArrayList<Pixel>();
-			for (int x = 0 ; x < r.getWidth() ; x++){ 
-				for (int y = 0 ; y < r.getHeight() ; y++){ 
-					Object objCouleur   = r.getDataElements(x, y, null);
-					pixels.add(new Pixel(x, y, new Couleur(cm.getRed(objCouleur), cm.getGreen(objCouleur), cm.getBlue(objCouleur))));
-				}	
-			}
-		}
-		return pixels;
-		
-	}
-	public void setPixels(ArrayList<Pixel> pixels){
-		this.pixels=pixels;
-		for (int x = 0 ; x < r.getWidth() ; x++){ 
-			for (int y = 0 ; y < r.getHeight() ; y++){ 
-				
-				buffImage.setRGB(x, y, this.pixels.get(y*r.getWidth()+x).getCouleurPixel().convertRGBIntoInt());
-				System.out.print(this.pixels.get(y*r.getWidth()+x).getCouleurPixel().convertRGBIntoInt());
-				System.out.print(buffImage.getRGB(x, y));
-			}	
-		}
 	}
 }
